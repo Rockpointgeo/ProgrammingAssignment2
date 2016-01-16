0d711b1e9f3d08f224f0424948169c126693db45
Rockpointgeo <blog@rockpointgeo.com>
2016-01-16 00:08:04 +0000

    New Code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains the functions written for the week 3
+##  R Programming Assignment
 
-## Write a short comment describing this function
+## This function creates a special matrix object that can cache 
+##  its inverse using the solve() function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_result <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_result <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) inv_result <<- inv
+    getinv <- function() inv_result
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
+## This function gets the inverse of the special matrix object
+##  created by makeCacheMatrix. If the inverse is NULL it calculates
+##  the invers using solve() otherwise it just retrieves the cached 
+##  value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv_result <- x$getinv()
+    if(!is.null(inv_result)) {
+        message("getting cached data")
+        return(inv_result)
+    }
+    data <- x$get()
+    inv_result <- solve(data, ...)
+    x$setinv(inv_result)
+    inv_result
+    
 }
